f9c121eadc554a9e2b1c4c9d33d9c959758c1e07
2haloes <rhysh@live.co.uk>
2024-06-17 12:11:13 +0100

    Add project files.

---
+++ FisherImageProject/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace FisherImageProject.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        // Replace this with something much better after understanding improves
+        public string Password { get; set; }
+    }
+}
---
+++ FisherImageProject/Models/UserDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace FisherImageProject.Models
+{
+    public class UserDTO
+    {
+        public long Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+    }
+}
